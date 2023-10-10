510591be04684f9b8e444afcd6faa114837c44d3
shjung <shjung@nextk.co.kr>
2023-10-10 17:49:56 +0900

    ﻿fix: 레코딩 날짜 조회 테스트 추가

---
+++ C#/NK_API_Test/SingleApiTests/Test_Record_days.cs
@@ -0,0 +1,28 @@
+﻿using NKAPIService.API.Channel;
+using NKAPIService.API.ComputingNode;
+
+namespace NK_API_Test.SingleApiTests
+{
+    internal class Test_Record_days : TestBase
+    {
+        internal async Task RecordDays()
+        {
+            while (true)
+            {
+                var firstCN = await service.Requset(new RequestGetComputingNode()
+                {
+
+                }) as ResponseGetComputingNode;
+
+                if (firstCN != null)
+                {
+                    var response = await service.Requset(new RequestRecordDays()
+                    {
+                        NodeId = firstCN.Node.NodeId
+
+                    }) as ResponseRecordDays;
+                }
+            }
+        }
+    }
+}
---
+++ C#/NK_API_Test/TestBase.cs
@@ -0,0 +1,15 @@
+﻿using NKAPIService;
+
+namespace NK_API_Test
+{
+    internal class TestBase
+    {
+        protected APIService service { get; }
+
+        public TestBase()
+        {
+            var uri = new Uri($"http://127.0.0.1:8880");
+            service = APIService.Build().SetUrl(uri);
+        }
+    }
+}
